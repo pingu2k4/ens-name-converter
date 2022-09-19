a2fcd9d84979214a14cad0a5dd7a417acdcec1bc
Matthew Parker <pingu2k4@hotmail.co.uk>
2022-09-19 01:17:29 +0100

    Retrieving hashes of full domain and label, in numeric and hex forms.

---
+++ EnsNameConverter/Program.cs
@@ -0,0 +1,35 @@
+﻿using Nethereum.ENS;
+using Nethereum.Hex.HexTypes;
+
+var ensUtil = new EnsUtil();
+
+Console.Write("Enter your domain name: ");
+var domain = Console.ReadLine()?.ToLower();
+
+if(string.IsNullOrEmpty(domain))
+{
+    throw new ArgumentException("domain cannot be null or empty", nameof(domain));
+}
+
+if(!domain.EndsWith(".eth"))
+{
+    domain = domain + ".eth";
+}
+
+var nameHash = ensUtil.GetNameHash(domain);
+
+var ggg = domain[..4];
+
+var labelHash = ensUtil.GetLabelHash(domain[..4]);
+
+var nameHashHex = new HexBigInteger(nameHash);
+
+var labelHashHex = new HexBigInteger(labelHash);
+
+Console.WriteLine($"Domain ID: {nameHashHex.Value}");
+Console.WriteLine($"Domain Hex: {nameHashHex.HexValue}");
+Console.WriteLine();
+Console.WriteLine("---------------------------------------------");
+Console.WriteLine();
+Console.WriteLine($"Label ID: {labelHashHex.Value}");
+Console.WriteLine($"Label Hex: {labelHashHex.HexValue}");
