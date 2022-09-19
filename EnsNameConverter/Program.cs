d7b585ece7bd10a2cfb9f39fdb06b38e0756ec40
Matthew Parker <pingu2k4@hotmail.co.uk>
2022-09-19 11:23:17 +0100

    Stuck it in a loop to make it more useful

--- EnsNameConverter/Program.cs
+++ EnsNameConverter/Program.cs
@@ -3,33 +3,39 @@ using Nethereum.Hex.HexTypes;
 
 var ensUtil = new EnsUtil();
 
-Console.Write("Enter your domain name: ");
-var domain = Console.ReadLine()?.ToLower();
-
-if(string.IsNullOrEmpty(domain))
+while (true)
 {
-    throw new ArgumentException("domain cannot be null or empty", nameof(domain));
+    Console.Clear();
+    Console.Write("Enter your domain name: ");
+    var domain = Console.ReadLine()?.ToLower();
+
+    if (string.IsNullOrEmpty(domain))
+    {
+        throw new ArgumentException("domain cannot be null or empty", nameof(domain));
+    }
+
+    if (!domain.EndsWith(".eth"))
+    {
+        domain = domain + ".eth";
+    }
+
+    var nameHash = ensUtil.GetNameHash(domain);
+
+    var labelHash = ensUtil.GetLabelHash(domain[..4]);
+
+    var nameHashHex = new HexBigInteger(nameHash);
+
+    var labelHashHex = new HexBigInteger(labelHash);
+
+    Console.WriteLine($"Domain ID: {nameHashHex.Value}");
+    Console.WriteLine($"Domain Hex: {nameHashHex.HexValue}");
+    Console.WriteLine();
+    Console.WriteLine("---------------------------------------------");
+    Console.WriteLine();
+    Console.WriteLine($"Label ID: {labelHashHex.Value}");
+    Console.WriteLine($"Label Hex: {labelHashHex.HexValue}");
+
+    Console.WriteLine();
+    Console.Write("Hit any key to start over: ");
+    Console.ReadKey();
 }
-
-if(!domain.EndsWith(".eth"))
-{
-    domain = domain + ".eth";
-}
-
-var nameHash = ensUtil.GetNameHash(domain);
-
-var ggg = domain[..4];
-
-var labelHash = ensUtil.GetLabelHash(domain[..4]);
-
-var nameHashHex = new HexBigInteger(nameHash);
-
-var labelHashHex = new HexBigInteger(labelHash);
-
-Console.WriteLine($"Domain ID: {nameHashHex.Value}");
-Console.WriteLine($"Domain Hex: {nameHashHex.HexValue}");
-Console.WriteLine();
-Console.WriteLine("---------------------------------------------");
-Console.WriteLine();
-Console.WriteLine($"Label ID: {labelHashHex.Value}");
-Console.WriteLine($"Label Hex: {labelHashHex.HexValue}");
